0d581f73f561baca01efcd1d0fcf43861fd56128
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-08-05 20:56:22 +0200

    modelo blogs, antes de migraciones

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+CREATED = 'ADD'
+PUBLISHED = 'PUB'
+CANCELED = 'CAN'
+BANNED = 'BAN'
+
+STATS = (
+    (CREATED, 'Created'),
+    (PUBLISHED, 'Published'),
+    (CANCELED, 'Canceled'),
+    (BANNED , 'Banned')
+)
+
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=150)
+    resumen = models.CharField(max_length=500)
+    cuerpo = models.TextField(blank=True, null=True, default="")
+    urlImg = models.URLField()
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=3, choices=STATS)
+
