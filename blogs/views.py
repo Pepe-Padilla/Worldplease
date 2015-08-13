2d047004c6f78a738ec5d9c0f4650707d47fd65f
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-08-13 23:38:09 +0200

    mejoras en permisos, listado y calidad, 404 auto

--- blogs/views.py
+++ blogs/views.py
@@ -5,8 +5,10 @@ from blogs.models import Blog
 from blogs.settings import PUBLISHED
 from blogs.forms import BlogForm
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.views.generic import View
+#from django.db.models import Q     # Q para OR en queries ... filter(Q(id=X) | Q(id=Y))
 
 class HomeView(View):
     def get(self, request):
@@ -17,19 +19,11 @@ class HomeView(View):
         """
         blogs = Blog.objects.filter(status=PUBLISHED).order_by('-created_at').select_related('owner')
 
-        titleHead = 'Home'
-        titleSection = 'Last WorldPlease publications'
-        title = {
-            'head': titleHead,
-            'section': titleSection
-        }
-
         context = {
             'blogs_list': blogs[:10],
-            'title': title
         }
 
-        return render(request, 'blogs/blog_list.html', context)
+        return render(request, 'blogs/home.html', context)
 
 class DetailView(View):
     def get(self, request, ownerName, pk):
@@ -39,13 +33,7 @@ class DetailView(View):
         :param pk: id blog
         :return: HttpResponse
         """
-        consulted_owner = User.objects.filter(username=ownerName)[0]
-
-        blog_req = Blog.objects.filter(
-            pk=pk,
-            owner=consulted_owner
-        ).select_related('owner')
-        blog = blog_req[0] if len(blog_req) >= 1 else None
+        blog = blogById(request, ownerName, pk)
 
         if blog is not None:
             #crear contexto
@@ -65,8 +53,11 @@ class AuthorView(View):
         :param ownerName: owner User
         :return: HttpResponse
         """
-        consulted_owner = User.objects.filter(username=ownerName)[0]
-        blogs = Blog.objects.filter(owner=consulted_owner).order_by('-created_at').select_related('owner')
+
+        blogs = blogsByOwner(request, ownerName)
+
+        if blogs is None:
+            return HttpResponseNotFound("Error 404 Not Found")
 
         titleHead = ownerName
         titleSection = 'Last WorldPlease publications of ' + ownerName
@@ -80,18 +71,30 @@ class AuthorView(View):
             'title': title
         }
 
-        return render(request, 'blogs/blog_list.html', context)
+        return render(request, 'blogs/author.html', context)
 
-@login_required()
-def create(request):
-    """
-    Shows a form to create a new blog
-    :param request: HttpRequest
-    :return: HttpRequest
-    """
-    if request.method == 'GET':
+class CreateView(View):
+    @method_decorator(login_required())
+    def get(self, request):
+        """
+        Shows a form to create a new blog
+        :param request: HttpRequest
+        :return: HttpRequest
+        """
         form = BlogForm()
-    else:
+
+        context = {
+            'form': form
+        }
+        return render(request, 'blogs/new_blog.html', context)
+
+    @method_decorator(login_required())
+    def post(self, request):
+        """
+        Shows a form to create a new blog
+        :param request: HttpRequest
+        :return: HttpRequest
+        """
         blog_with_owner = Blog()
         blog_with_owner.owner = request.user
         form = BlogForm(request.POST, instance=blog_with_owner)
@@ -99,7 +102,55 @@ def create(request):
             new_blog = form.save() # Guarda el objeto y lo devuelve FTW
             return redirect('blog_detail', ownerName=new_blog.owner, pk=new_blog.pk)
 
-    context = {
-        'form': form
-    }
-    return render(request, 'blogs/new_blog.html', context)
+        context = {
+            'form': form
+        }
+        return render(request, 'blogs/new_blog.html', context)
+
+class NotFoundView(View):
+    def get(self, request):
+        return HttpResponseNotFound("Error 404 Not Found")
+    def post(self, request):
+        return HttpResponseNotFound("Error 404 Not Found")
+
+
+def blogsByOwner(request, ownerName):
+    """
+    Función que regresa los blogs de un usuario dependiendo del usuario autenticado
+    - No autenticado u otro usuario: publicas
+    - Autenticado y autor: todas
+    :param request: HttpRequest
+    :param ownerName: user consulted (string)
+    :return: Blog List
+    """
+    consulted_owner = User.objects.filter(username=ownerName)
+
+    if len(consulted_owner) < 1:
+        return None
+
+    consulted_owner = consulted_owner[0]
+
+    if request.user.username == ownerName or request.user.is_superuser:
+        blogs = Blog.objects.filter(owner=consulted_owner).order_by('-created_at').select_related('owner')
+    else:
+        blogs = Blog.objects.filter(owner=consulted_owner, status=PUBLISHED).order_by('-created_at').select_related('owner')
+
+    return blogs
+
+
+def blogById(request, ownerName, pk):
+
+    if request.user.username == ownerName or request.user.is_superuser:
+        blog_req = Blog.objects.filter(
+            pk=pk,
+            owner__username=ownerName,
+        ).select_related('owner')
+    else:
+        blog_req = Blog.objects.filter(
+            pk=pk,
+            owner__username=ownerName,
+            status=PUBLISHED
+        ).select_related('owner')
+    blog = blog_req[0] if len(blog_req) >= 1 else None
+
+    return blog
--- worldplease/urls.py
+++ worldplease/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from blogs.views import HomeView, DetailView, AuthorView
+from blogs.views import HomeView, DetailView, AuthorView, CreateView, NotFoundView
 from users.views import LoginView, LogoutView
+#from django.http import HttpResponseNotFound
 
 
 urlpatterns = [
@@ -11,12 +12,12 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='blog_home'),
     url(r'^blogs/(?P<ownerName>[a-zA-Z0-9]+)/(?P<pk>[0-9]+)$', DetailView.as_view(), name='blog_detail'),
     url(r'^blogs/(?P<ownerName>[a-zA-Z0-9]+)$', AuthorView.as_view(), name='blog_owner'),
-    url(r'^blogs/_new$', 'blogs.views.create', name='blog_create'),
+    url(r'^blogs/_new$', CreateView.as_view(), name='blog_create'),
 
     # Users urls
     url(r'^login$', LoginView.as_view(), name='users_login'),
     url(r'^logout$', LogoutView.as_view(), name='users_logout'),
 
     #when other (error 404)
-    #url(r'^.*$', '')
+    url(r'^.*$', NotFoundView.as_view(), name='not_found_404'),
 ]
