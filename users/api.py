7e76abe685f51ef0a574914f380a43501b8cbd0f
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-08-25 21:38:50 +0200

    alta de usuairos RES

--- users/api.py
+++ users/api.py
@@ -4,6 +4,7 @@ from users.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 class UserListlAPI(APIView):
 
@@ -11,6 +12,14 @@ class UserListlAPI(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserDetailAPI(APIView):
 
